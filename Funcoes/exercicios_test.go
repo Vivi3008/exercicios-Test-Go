5dafeab5451b5413aa8b89436b4c29e38061a4f2
Vivi Ramos <ramosluzviviane@gmail.com>
2021-12-22 19:30:38 -0300

    add test ex 3

--- Funcoes/exercicios_test.go
+++ Funcoes/exercicios_test.go
@@ -2,6 +2,7 @@ package funcoes
 
 import (
 	"testing"
+	"time"
 )
 
 func TestExerciciosFuncoes(t *testing.T) {
@@ -20,6 +21,16 @@ func TestExerciciosFuncoes(t *testing.T) {
 			t.Errorf("Expected Olar, got %s", cumprimento)
 		}
 
+	})
+
+	t.Run("Ex 4 Printar cumprimentdo por periodo", func(t *testing.T) {
+		date := time.Date(2019, 6, 5, 11, 51, 04, 0, time.UTC)
+		cumpr := SayHelloPeriod("Viviane", date)
+		expected := "Bom Dia Viviane"
+
+		if cumpr != expected {
+			t.Errorf("Expected %s, got %s", expected, cumpr)
+		}
 	})
 
 }
